f1735d4102fed518f9d2efdad987af62245d5dc2
Josh Rio <github-41c6@protonmail.com>
2021-11-30 19:05:18 -0400

    Updates

---
+++ precache-manifest.c267e2dba861086d2df49ddc15165c11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c92fdf6c8817401e20de92d8870ce2d",
+    "url": "/%20blockchain-developer-bootcamp-project/index.html"
+  },
+  {
+    "revision": "f63255bdaebc84d42761",
+    "url": "/%20blockchain-developer-bootcamp-project/static/js/2.e89a282f.chunk.js"
+  },
+  {
+    "revision": "7ca9147a6a73b9b4caac",
+    "url": "/%20blockchain-developer-bootcamp-project/static/js/main.928733b1.chunk.js"
+  },
+  {
+    "revision": "9f4bb6dbdc6abfa6d1b0",
+    "url": "/%20blockchain-developer-bootcamp-project/static/js/runtime-main.224890d9.js"
+  },
+  {
+    "revision": "180cdf144e73eda4ee3cd2e4df3810d4",
+    "url": "/%20blockchain-developer-bootcamp-project/static/media/admin.180cdf14.svg"
+  },
+  {
+    "revision": "3563e553a600693a09d9ee03917c38bc",
+    "url": "/%20blockchain-developer-bootcamp-project/static/media/empty.3563e553.svg"
+  },
+  {
+    "revision": "36e2501698757ca452d733c4f1d72885",
+    "url": "/%20blockchain-developer-bootcamp-project/static/media/metamask.36e25016.svg"
+  },
+  {
+    "revision": "efd776edc0d65337dc3583160d821cfa",
+    "url": "/%20blockchain-developer-bootcamp-project/static/media/player.efd776ed.svg"
+  },
+  {
+    "revision": "245f93c497f1e06f1d71ee775486d49e",
+    "url": "/%20blockchain-developer-bootcamp-project/static/media/powerballer.245f93c4.svg"
+  }
+]);
