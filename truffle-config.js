ee05a7ec633ab3c1cb8975c87256b7ffa2d7f6fe
Josh Rio <github-41c6@protonmail.com>
2021-11-30 15:49:07 -0400

    Prepare to deploy to ropsten with env files

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,34 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
+
+// let deployWallet = new HDWalletProvider({
+//   mnemonic: {
+//     phrase: process.env.MNEMONIC,
+//   },
+//   providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+// });
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
   networks: {
     develop: {
       port: 8545,
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider({
+          mnemonic: {
+            phrase: process.env.MNEMONIC,
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        });
+      },
+      gasPrice: 100000000000000000,
+      network_id: 3,
     },
   },
   // compilers: {
